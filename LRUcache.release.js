257c1e61e49240d9952a71c662e0bf6da326bace
yashiro_ryo <yashiro2000185@yahoo.co.jp>
2021-08-05 02:37:50 +0900

    add comment

--- LRUcache.release.js
+++ LRUcache.release.js
@@ -1,24 +1,30 @@
-let cache1 = [0, 0];
-let cache2 = [0, 0];    //cache array
+//LRUcache program
+let cache1 = [0, 0];	//キャッシュ用配列
+let cache2 = [0, 0];	//2が一番最近にアクセスされたキャッシュ
+//LRU クラス
 class LRU{
-    //put method
-    //insert data
+    //putメソッド
     put(key, value){
+    	//引数はcache2から代入
         cache1 = cache2;
         cache2 = [key, value];
     }
-    //get method
-    //get data
+    //getメソッド
     get(key){
+    	//keyが一番新しいキャッシュと一致する場合
         if(cache2[0] == key){
+            //一番新しいキャッシュにアクセスしたので、順番に変更なし
             console.log(cache2[1]);
         }
+        //keyが2番目のキャッシュと一致する場合
         else if(cache1[0] == key){
+            //一番新しいキャッシュが更新される
             let cache3 = cache1;
             cache1 = cache2;
             cache2 = cache3;
             console.log(cache2[1]);
         }
+        //キャッシュに該当なし(すでに上書きされた)
         else{
             console.log("null");
         }
