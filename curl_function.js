42ef0bcdd0c3ee5ef5a8462f0f9e909e895d4337
yashiro_ryo <yashiro2000185@yahoo.co.jp>
2021-08-13 00:16:02 +0900

    各オプションの処理の雛形を追加

--- curl_function.js
+++ curl_function.js
@@ -1,8 +1,25 @@
 const args = process.argv;
 let cnt = 2;
-
+//ファイルに出力する処理
+function output(){
+}
+//リクエスト処理部分****************************************************
+function request(){
+}
+//outputオプション処理部分******************************************
+function output_option(args, cnt){
+}
+//view-headerオプション処理部分*************************************
+function view-header(args, cnt){
+}
+//Xオプション処理部分***********************************************
+function X-option(args, cnt){
+}
+//dataオプション処理部分********************************************
+function data-option(args, cnt){
+}
 //コマンドオプション判定部分
-function command_option(args){
+function command_option(args, cnt){
 	if(args[cnt] == '-o'||args[cnt] == '--output'){
 		console.log("-oオプション実装部分");
 	}
@@ -17,5 +34,8 @@ function command_option(args){
 	}
 	else if(args[cnt] == '-h'||args[cnt] == '--help'){
 		console.log("-hオプション実装部分");
+	}
+	else{
+		console.log("オプション無し実装部分");
 	}
 }
