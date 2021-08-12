6b24d586e7d2257b8968da8391750549cd4c9736
yashiro_ryo <yashiro2000185@yahoo.co.jp>
2021-08-13 00:08:32 +0900

    オプション判定部分の雛形を作成

--- curl_function.js
+++ curl_function.js
@@ -1,6 +1,21 @@
 const args = process.argv;
 let cnt = 2;
-console.log(args[cnt]);
-if(args[cnt] == '-e'||args[cnt] == '--example'){
-	console.log("-e option");
+
+//コマンドオプション判定部分
+function command_option(args){
+	if(args[cnt] == '-o'||args[cnt] == '--output'){
+		console.log("-oオプション実装部分");
+	}
+	else if(args[cnt] == '-v'||args[cnt] == '--view-header'){
+		console.log("-vオプション実装部分");
+	}
+	else if(args[cnt] == '-X'||args[cnt] == '--request'){
+		console.log("-Xオプション実装部分");
+	}
+	else if(args[cnt] == '-d'||args[cnt] == '--data'){
+		console.log("-dオプション実装部分");
+	}
+	else if(args[cnt] == '-h'||args[cnt] == '--help'){
+		console.log("-hオプション実装部分");
+	}
 }
